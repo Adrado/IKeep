11c82af6b08331ccdd6ce63d6285dddd7538bd22
Fidel <fidel.mh3@gmail.com>
2019-10-14 13:44:41 +0200

    TreeNode A1

--- IKeep.Lib.Server/Services/TreeViewService.cs
+++ IKeep.Lib.Server/Services/TreeViewService.cs
@@ -30,6 +30,7 @@ namespace IKeep.Lib.Server.Services
                     .Select(t => new TreeNode { Id = t.Id, ParentId = t.InstallationId, Name = t.Name })
                     .ToDictionary(t => t.Id);
 
+
                 // Create a new root node
                 tree.RootNode = new TreeNode { Id = Guid.NewGuid(), Name = "Root" };
 
