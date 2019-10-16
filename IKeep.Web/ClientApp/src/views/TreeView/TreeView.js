4f73cd186ab88de00c519a3bcc450fb94dc62fc7
Fidel <fidel.mh3@gmail.com>
2019-10-16 10:29:52 +0200

    TreeView OK

--- IKeep.Web/ClientApp/src/views/TreeView/TreeView.js
+++ IKeep.Web/ClientApp/src/views/TreeView/TreeView.js
@@ -1,39 +1,25 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import {Treebeard} from 'react-treebeard';
+import {Treebeard} from 'react-treebeard'
 
-const data = {
-    name: 'root',
-    toggled: true,
-    children: [
-        {
-            name: 'parent',
-            children: [
-                { name: 'child1' },
-                { name: 'child2' }
-            ]
-        },
-        {
-            name: 'loading parent',
-            loading: true,
-            children: []
-        },
-        {
-            name: 'parent',
-            children: [
-                {
-                    name: 'nested parent',
-                    children: [
-                        { name: 'nested child 1' },
-                        { name: 'nested child 2' }
-                    ]
-                }
-            ]
-        }
-    ]
-};
 
-class TreeExample extends Component 
+
+let TreeView = props =>
+{
+    //const classes = useStyles();
+    const {Data, onToggle} = props;
+    
+    return(
+        <Treebeard
+            data = {Data}
+            onToggle = {onToggle}
+        />
+    );
+}
+
+export default TreeView;
+
+/* class TreeExample extends Component 
 {
     constructor(props)
     {
@@ -44,17 +30,6 @@ class TreeExample extends Component
         console.log(this.props)
     }
     
-    onToggle(node, toggled){
-        const {cursor, data} = this.state;
-        if (cursor) {
-            this.setState(() => ({cursor, active: false}));
-        }
-        node.active = true;
-        if (node.children) { 
-            node.toggled = toggled; 
-        }
-        this.setState(() => ({cursor: node, data: Object.assign({}, data)}));
-    }
     
     render()
     {
@@ -62,12 +37,12 @@ class TreeExample extends Component
         return (
             <Treebeard
                 data={this.Data}
-                /* onToggle={this.onToggle} */
+                onToggle={this.onToggle} 
             />
         );
     }
 }
-export default TreeExample;
+export default TreeExample; */
 /* const content = document.getElementById('content');
 ReactDOM.render(<TreeExample/>, content); */
 //https://github.com/storybookjs/react-treebeard
--- IKeep.Web/ClientApp/src/views/TreeView/TreeViewModel.js
+++ IKeep.Web/ClientApp/src/views/TreeView/TreeViewModel.js
@@ -15,12 +15,10 @@ class TreeViewModel extends Component
     {
         super(props);
         this.TreeViewService = new TreeViewService();
-
         this.InstallationsService = new InstallationsService();
         this.BuildingsService = new BuildingsService();
         this.FloorsService = new FloorsService();
         this.AreasService = new AreasService();
-
         this.Data = {};
         this.GetTreeView();
         this.TreeViewData = [];
@@ -29,9 +27,7 @@ class TreeViewModel extends Component
         {
             vocal: ''
         }
-        
-
-        
+        this.onToggle = this.onToggle.bind(this);
     }
 
     GetTreeView()
@@ -46,42 +42,28 @@ class TreeViewModel extends Component
     OnGetData(response)
     {
         this.Data = response.data.rootNode;
-        console.log(response);
-        console.log("Ahora?")
-        console.log(this.Data);
         this.setState({vocal : 'b'})
         //this.CleanForm();
     } 
 
-    /* GetAllInstallations()
-    {
-        this.InstallationsService.GetAllAsync()
-        .then((response) =>
-            {
-                console.log(response);
-                this.OnGetData(response);
-            });
-    } */
-
-    /* OnGetData(response)
-    {
-        this.Installations.length = 0;
-        for (let i in response.data)
-        {
-            let installation = new Installation(response.data[i]);
-            
-            if(installation.EntityStatus !== 0)  
-                this.Installations.push(installation);
+    onToggle(node, toggled){
+        const {cursor, data} = this.state;
+        if (cursor) {
+            this.setState(() => ({cursor, active: false}));
         }
-    } */
+        node.active = true;
+        if (node.children) { 
+            node.toggled = toggled; 
+        }
+        this.setState(() => ({cursor: node, data: Object.assign({}, data)}));
+    }
 
     render()
     {
-        console.log("x");
-        console.log(this.Data);
         return(
             <TreeExample
             Data = {this.Data}
+            onToggle = {this.onToggle}
             />
         );
     }
