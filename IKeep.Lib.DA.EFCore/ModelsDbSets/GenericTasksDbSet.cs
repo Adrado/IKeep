a29c3ee03683ac29a6e8d255bd64934917f055be
Fidel <fidel.mh3@gmail.com>
2019-11-13 12:03:50 +0100

    Add GenericTasksDbSet

---
+++ IKeep.Lib.DA.EFCore/ModelsDbSets/GenericTasksDbSet.cs
@@ -0,0 +1,16 @@
+﻿using IKeep.Lib.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IKeep.Lib.DA.EFCore
+{
+    public class GenericTasksDbset : IKeepDbSet<GenericTask>
+    {
+        public GenericTasksDbset(IKeepContext dbContext)
+        {
+            DbContext = dbContext;
+            DbSet = dbContext.GenericTasks;
+        }
+    }
+}
