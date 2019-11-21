01aab54d0c8f2b67c452774bd4fcc9b8218989f3
Fidel <fidel.mh3@gmail.com>
2019-11-21 13:31:55 +0100

    Add Spanish Localization

---
+++ IKeep.Web/ClientApp/src/components/MaterialTableProps.js
@@ -0,0 +1,32 @@
+export const localizationEsp ={
+    header: {
+      actions: ' '
+    },
+    body: {
+      emptyDataSourceMessage: 'No se han encontrado coincidencias',
+      filterRow:{
+        filterTooltip: 'Filtrar'
+      },
+      editRow:{
+        deleteText: '¿Estás seguro de eliminar esta fila?',
+        cancelTooltip: 'Cancelar',
+        saveTooltip: 'Guardar'
+      },
+      editTooltip: 'Editar',
+      deleteTooltip: 'Eliminar'
+    },
+    toolbar: {
+      searchTooltip: 'Buscar',
+      searchPlaceholder: 'Buscar'
+    },
+    pagination: {
+      labelRowsSelect: 'Filas',
+      labelDisplayedRows: ' {from}-{to} de {count}',
+      firstTooltip: 'Primera página',
+      previousTooltip: 'Anterior página',
+      nextTooltip: 'Siguiente página',
+      lastTooltip: 'Última página'
+    }
+  }
+
+  
