c6c616efee5fc5df6eed528ec7f135bfea48f922
Fidel <fidel.mh3@gmail.com>
2019-12-03 11:45:43 +0100

    refactor: delete unnecessary code

--- IKeep.Web/ClientApp/src/App.js
+++ IKeep.Web/ClientApp/src/App.js
@@ -1,17 +1,8 @@
 import React from 'react';
-import { Route } from 'react-router';
-import Layout from './components/Layout';
-import Home from './components/Home';
-import InstallationManagerView from './views/InstallationManager/InstallationManagerView';
-import GenericElementView from './views/GenericElement/GenericElementView'
-import ElementTypeView from './views/ElementType/ElementTypeView'
-
-import {Services, ElementTypeService , CRUD} from './providers/Providers';
 import Layout2 from './components/Layout2';
 
 const App = () => (
   <Layout2></Layout2>
-  
 );
 
 export default App;
