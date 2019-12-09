0b9fff1dc56af8b6e89be0e8f65380cec2c7a22a
Fidel <fidel.mh3@gmail.com>
2019-12-09 13:42:06 +0100

    refactor: allows to use getbyid for diferents ids (user, installation)

--- IKeep.Web/ClientApp/src/services/InstallationUsersService.js
+++ IKeep.Web/ClientApp/src/services/InstallationUsersService.js
@@ -5,6 +5,16 @@ class InstallationUsersService extends CRUDService
     {
         super("installationUsers");
     }
+
+    GetByInstallationIdAsync(installationId)
+    {
+        return this.GetByIdAsync("installation/" + installationId);
+    }
+
+    GetByUserIdAsync(userId)
+    {
+        return this.GetByIdAsync("user/" + userId);
+    }
 }
 
 export default InstallationUsersService;
