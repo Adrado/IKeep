07c6bf1783581c5962aacdff4649a69161255dc5
Fidel <fidel.mh3@gmail.com>
2019-12-13 13:47:34 +0100

    refactor: Allows to get all elements of a installation

--- IKeep.Lib.Server/Services/ChoreService.cs
+++ IKeep.Lib.Server/Services/ChoreService.cs
@@ -14,7 +14,7 @@ namespace IKeep.Lib.Server.Services
     {
         // private readonly ICrudService<Chore> _tasksService;
         // private readonly ICrudService<Installation> _installationsService;
-        //private readonly ICrudService<Building> _buildingsService;
+        // private readonly ICrudService<Building> _buildingsService;
         // private readonly ICrudService<Floor> _floorsService;
         // private readonly ICrudService<Area> _areasService;
         // private readonly ICrudEntity<Element> _elementsService;
@@ -40,25 +40,63 @@ namespace IKeep.Lib.Server.Services
             _context = context;
         }
 
-        public IEnumerable<Installation> AddChoresToInstallation(Guid installationId)
+        public IEnumerable<Element> AddChoresToInstallation(Guid installationId)
         {
-            var installation = new List<Installation>();
-            using(var db = _context)
-            {
-                var elements = new List<Element>();
-                var FullInstallation = db.Installations
-                    .Where(i => i.Id == installationId)
-                    .Include(i => i.Buildings)
-                    .ThenInclude(b => b.Floors)
-                    .ThenInclude(f => f.Areas)
-                    .ThenInclude(a => a.Elements)
-                    .ThenInclude(e => e.Chores)
-                    .ToList();
+            var elements = new List<Element>();
+                elements = (from Installation in _context.Installations
+                                                    //.Include(i => i.Buildings)
+                                                    //.ThenInclude(b => b.Floors)
+                                                    //.ThenInclude(f => f.Areas)
+                                                    //.ThenInclude(a => a.Elements)
+                            where Installation.Id == installationId
+                                 from Building in Installation.Buildings
+                                 from Floor in Building.Floors
+                                 from Area in Floor.Areas
+                                 from Element in Area.Elements
+                                 select Element).ToList();
 
-                installation = FullInstallation;
-            }
-            return installation;
+            return elements;
         }
+
+        private void AddChores(IEnumerable<Element> elements)
+        {
+            foreach(Element element in elements)
+            {
+                AddChore(element);
+            }
+        }
+
+        private void AddChore(Element element)
+        {
+            var elementGenericChores = element.ElementGenericChores;
+            foreach(ElementGenericChore elemenGChore in elementGenericChores)
+            {
+                GenericChore gChore = elemenGChore.GenericChore;
+                Period gChorePeriod = gChore.Period;
+                AddTasks(element, gChore, gChorePeriod);
+            }
+        }
+
+        private void AddTasks(Element element, GenericChore gChore, Period gChorePeriod)
+        {
+
+        }
+
+
+        private IEnumerable<Element> AddChoresToBuilding(Guid buildingId)
+        {
+            var elements = new List<Element>();
+            elements = (from Building in _context.Buildings     
+                        where Building.Id == buildingId
+                        from Floor in Building.Floors
+                        from Area in Floor.Areas
+                        from Element in Area.Elements
+                        select Element).ToList();
+
+            return elements;
+        }
+
+
 
         private bool AddNewTask(Element element, GenericChore genericChore, DateTime prevDate)
         {
@@ -67,7 +105,7 @@ namespace IKeep.Lib.Server.Services
             chore.StartDate = prevDate.AddDays(1);
             chore.EndDate = chore.StartDate.AddDays(1);
             chore.GenericChoreId = genericChore.Id;
-
+            _choresService.Add(chore);
             return true;
         }
 
@@ -118,3 +156,28 @@ namespace IKeep.Lib.Server.Services
         //}
     }
 }
+
+
+
+//using (var db = _context)
+//{
+
+//var installation = db.Installations.Where(i => i.Id == installationId).FirstOrDefault();
+//var hola = installation.Buildings.SelectMany(building => building.Floors).SelectMany(floor => floor.Areas).SelectMany(area => area.Elements); ;
+//var FullInstallation = db.Installations
+//    .Where(i => i.Id == installationId)
+//    .Include(i => i.Buildings)
+//    .ThenInclude(b => b.Floors)
+//    .ThenInclude(f => f.Areas)
+//    .ThenInclude(a => a.Elements)
+//    .ThenInclude(e => e.Chores)
+//    .ToList();
+
+//var FullInstallation = db.Installations
+//    .Where(i => i.Id == installationId)
+//    .Include(i => i.Buildings)
+//    .ThenInclude(b => b.Floors)
+//    .ThenInclude(f => f.Areas)
+//    .ThenInclude(a => a.Elements)
+
+//    .ToList();
