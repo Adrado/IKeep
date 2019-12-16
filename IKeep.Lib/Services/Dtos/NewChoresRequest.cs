1573e3cfc5d44eeb6bbea769856227518288a432
Fidel <fidel.mh3@gmail.com>
2019-12-16 13:42:14 +0100

    feat: Adds NewChoreRequest dto

---
+++ IKeep.Lib/Services/Dtos/NewChoresRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IKeep.Lib.Services.Dtos
+{
+    public class NewChoresRequest
+    {
+        public Guid? InstallationId { get; set; }
+        public Guid? BuildingId { get; set; }
+        public Guid? FloorId { get; set; }
+        public Guid? AreaId { get; set; }
+        public int Year { get; set; }
+        public int? Month { get; set; }
+        public int? Day { get; set; }
+    }
+}
