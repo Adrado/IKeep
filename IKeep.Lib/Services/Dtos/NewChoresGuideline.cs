37da8477864c4929fc2ff65fb15a9e1b8f7e8290
Fidel <fidel.mh3@gmail.com>
2019-12-18 13:44:59 +0100

    feat: Adds new Dtos to simplify Chore Service

---
+++ IKeep.Lib/Services/Dtos/NewChoresGuideline.cs
@@ -0,0 +1,14 @@
+﻿using IKeep.Lib.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IKeep.Lib.Services.Dtos
+{
+    public class NewChoresGuideline
+    {
+        public Guid ElementId { get; set; }
+        public GenericChore GenericChore { get; set; }
+        public TimeSpanToAddChores TimeSpanToAddChores { get; set; }
+    }
+}
---
+++ IKeep.Lib/Services/Dtos/TimeSpanToAddChores.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IKeep.Lib.Services.Dtos
+{
+    public class TimeSpanToAddChores
+    {
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+    }
+}
