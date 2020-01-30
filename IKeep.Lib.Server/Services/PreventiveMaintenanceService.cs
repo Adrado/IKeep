6b9a7c4b803d05ecb3be5b5edd4d8278eb46feac
Fidel <fidel.mh3@gmail.com>
2020-01-30 13:48:35 +0100

    refactor: Creates multilevel list to render into a table

--- IKeep.Lib.Server/Services/PreventiveMaintenanceService.cs
+++ IKeep.Lib.Server/Services/PreventiveMaintenanceService.cs
@@ -12,25 +12,82 @@ namespace IKeep.Lib.Server.Services
     public class PreventiveMaintenanceService : IPreventiveMaintenanceService
     {
         private readonly ICrudService<Chore> _choresService;
-        public PreventiveMaintenanceService(ICrudService<Chore> choresService)
+        private readonly ICrudService<Element> _elementsService;
+        public PreventiveMaintenanceService(ICrudService<Chore> choresService,
+                                            ICrudService<Element> elementsService)
         {
             _choresService = choresService;
+            _elementsService = elementsService;
         }
 
-        public List<Chore> GetCurrentChoresForToday(MaintenanceRequest request)
+        public PreventiveMaintenanceData PreventiveMaintenanceData { get; set; }
+
+
+        public PreventiveMaintenanceData GetDataToTable(MaintenanceRequest request)
+        {
+            PreventiveMaintenanceData = new PreventiveMaintenanceData();
+            List<Chore> totalChores = GetCurrentChoresForToday(request);
+            var choresFiltered = TotalFiltered(totalChores);
+            var choresByElementList = GetChoresByElement(choresFiltered);
+            GetChoresByTypeAndPeriod(choresByElementList);
+            return PreventiveMaintenanceData;
+        }
+
+        private List<Chore> GetCurrentChoresForToday(MaintenanceRequest request)
         {
             return _choresService.GetAll()
-                .Where(c => c.Element.Area.Floor.Building.Installation.Id == request.InstallationId 
-                && ( DateTime.Compare(c.StartDate, request.Date) <= 0 
+                .Where(c => c.Element.Area.Floor.Building.Installation.Id == request.InstallationId
+                && (DateTime.Compare(c.StartDate, request.Date) <= 0
                 && DateTime.Compare(c.EndDate, request.Date) >= 0))
                 .ToList();
         }
 
-        //List<Chore> todayChores = GetCurrentChores(allChores, request.Date);
+        public List<List<Chore>> TotalFiltered(List<Chore> totalChores)
+        {
+            var filtered = totalChores
+                .GroupBy(x => new { x.Type, x.Period, x.Element.Name})
+                .Select(grp => grp.ToList())
+                .ToList();
 
-        //private List<Chore> GetCurrentChores(List<Chore> chores, DateTime date)
-        //{
-        //    return chores.Where(c => DateTime.Compare(c.StartDate, date) <= 0 && DateTime.Compare(c.EndDate, date) >= 0 ).ToList();
-        //}
+            return filtered;
+        }
+
+        private List<ChoresByElement> GetChoresByElement(List<List<Chore>> filtered)
+        {
+            List<ChoresByElement> choresByElementList = new List<ChoresByElement>();
+            foreach (List<Chore> chores in filtered)
+            {
+                ChoresByElement choresByElement = new ChoresByElement()
+                {
+                    ElementName = chores.First().Element.Name,
+                    Period = chores.First().Period,
+                    ChoreTypeName = chores.First().Type,
+                    NumChores = chores.Count(),
+                    Chores = chores,
+                };
+                choresByElementList.Add(choresByElement);
+            }
+            return choresByElementList;
+        }
+        private void GetChoresByTypeAndPeriod(List<ChoresByElement> choresByElementsList)
+        {
+
+            var response = choresByElementsList
+                .GroupBy(x => new { x.Period, x.ChoreTypeName })
+                .Select(grp => grp.ToList())
+                .ToList();
+
+            foreach(List<ChoresByElement> list in response)
+            {
+                ChoresByTypeAndPeriod choresByTypeAndPeriod = new ChoresByTypeAndPeriod()
+                {
+                    Period = list.First().Period,
+                    ChoreTypeName = list.First().ChoreTypeName,
+                    numChores = list.Sum(x => x.NumChores),
+                    TypeAndPeriodChores = list,
+                };
+                PreventiveMaintenanceData.TableData.Add(choresByTypeAndPeriod);
+            }
+        }
     }
 }
